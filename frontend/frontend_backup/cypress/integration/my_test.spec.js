e1d790ed7fc388a535093643d60bf257126e67df
jyri-kemppainen <56248441+jyri-kemppainen@users.noreply.github.com>
2022-04-17 13:32:41 +0300

    Delete frontend/frontend_backup directory

--- frontend/frontend_backup/cypress/integration/my_test.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Places app E2E test', function() {
-    it('front page can be opened', function() {   
-      let url = Cypress.env('URL')
-      cy.visit(url)
-      cy.contains('Places App')
-      cy.contains('Login')
-      cy.contains("Radu's favorite beach")
-    })
-})
